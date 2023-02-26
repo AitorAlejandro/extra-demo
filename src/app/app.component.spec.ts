0401670f7cb7d5252d184e88038300f6400db702
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2023-02-26 18:35:39 +0100

    translation infrastructure

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,39 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import {
+  TranslateLoader,
+  TranslateModule,
+  TranslateService,
+} from '@ngx-translate/core';
+import { HttpLoaderFactory } from './app.module';
+import { HttpClient } from '@angular/common/http';
 
 describe('AppComponent', () => {
+  let translate: TranslateService;
+  let http: HttpTestingController;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient],
+          },
+        }),
+      ],
       declarations: [AppComponent],
     }).compileComponents();
+    translate = TestBed.get(TranslateService);
+    http = TestBed.get(HttpTestingController);
   });
 
   it('should create the app', () => {
